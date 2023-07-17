7fff3f1a286885abd7d05f67203085f5ca1d58f4
Micah Harper <awareness1975@gmail.com>
2023-07-16 19:44:53 -0500

    added js included a font size button

--- isaiah53.js
+++ isaiah53.js
@@ -1,1 +1,12 @@
+let head = document.getElementById("Jesus");
+let i = 0;
+let myArray = ['40px' , '43px' , '45px' ];
+
+function increaseFontSize () {
+   head.style.fontSize = myArray[i++];
+}
+
+let theBtn = document.getElementById("increase");
+theBtn.addEventListener("click", increaseFontSize)
+
 
