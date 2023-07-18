84a16d857a8efcab28783031b5e3975597451286
Micah Harper <awareness1975@gmail.com>
2023-07-17 20:01:42 -0500

    changed js to make increase decrease work correctly

--- isaiah53.js
+++ isaiah53.js
@@ -3,7 +3,9 @@ let i = 0;
 let myArray = ['43px' , '47px' , '50px' ];
 
 function increaseFontSize () {
-   head.style.fontSize = myArray[i++];
+   if ( i <= 2) {
+   head.style.fontSize = myArray[i++];}
+   m = 0
 }
 
 let theBtn = document.getElementById("increase");
@@ -12,8 +14,12 @@ theBtn.addEventListener("click", increaseFontSize)
 let m = 0
 let theArray = ['37px', '33px' , '29px'];
 function decreaseFontSize () {
-    head.style.fontSize = theArray[m++]
+    if ( m <= 2) {
+    i = 0
+    head.style.fontSize = theArray[m++] }
 }
 let myBtn = document.getElementById("decrease");
 myBtn.addEventListener("click", decreaseFontSize)
+
+
 
